9d7bdefb38f98e1ec8dea809ef631d5ed0a8a17e
Mohamed Saqer <mohamed.saqer@outlook.com>
2018-02-26 02:08:05 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "FrontCompression.h"
+using namespace std;
+
+int main(){
+    FrontCompression frontCompression;
+    frontCompression.compress("phonelist.txt");
+
+    return 0;
+}
